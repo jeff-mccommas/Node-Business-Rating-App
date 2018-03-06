04be6f7b933c99e2af64caa44ead14ca9e86248d
jeff-mccommas <jeffmccommas@gmail.com>
2018-03-06 11:20:10 -0500

    added mongo store

--- server/server.js
+++ server/server.js
@@ -5,12 +5,24 @@ const cookieParser = require('cookie-parser');
 const ejs = require('ejs');
 const engine = require('ejs-mate');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const app = express();
+mongoose.connect('mongodb://localhost/rateme');
 app.engine('ejs',engine);
 app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+app.use(session({
+   secret:'bluebird',
+   resave:false,
+   saveUninitialized:false
+}))
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.render('index')
+    
+});
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
