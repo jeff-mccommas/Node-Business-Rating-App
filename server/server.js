5176b016456ff8916993334de5411c52ad39cfbb
jeff-mccommas <jeffmccommas@gmail.com>
2018-03-06 06:23:16 -0500

    app engine

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const hbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const ejs = require('ejs');
+const engine = require('ejs-mate');
+const session = require('express-session');
 const app = express();
+app.engine('ejs',engine);
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
