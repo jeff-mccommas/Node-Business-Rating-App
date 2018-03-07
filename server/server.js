2706ad279b82c47bdbc807ac605f876c93375195
jeff-mccommas <jeffmccommas@gmail.com>
2018-03-07 09:49:38 -0500

    refactored code

--- server/server.js
+++ server/server.js
@@ -1,22 +1,37 @@
+
 const express = require('express');
+const hbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const ejs = require('ejs');
-const engine = require('ejs-mate');
+const moment = require('moment');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const app = express();
-mongoose.connect('mongodb://localhost/rateme');
-app.engine('ejs',engine);
-app.set('view engine', 'ejs');
+const config = require('./config/config').get(process.env.NODE_ENV);
+////######### HBS SETUP ############/////
+app.engine('hbs',hbs({
+    extname:'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + './../views/layouts',
+    partialsDir: __dirname + './../views/partials'
+}));
+app.set('view engine','hbs')
+////######### DB ############/////
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DATABASE);
+
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
 app.use(session({
    secret:'bluebird',
    resave:false,
-   saveUninitialized:false
+   saveUninitialized:false,
+   store: new MongoStore({
+       mongooseConnection:mongoose.connection
+   })
 }))
 app.use(express.static('public'));
 app.get('/', (req, res) => {
