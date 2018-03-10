caccca83a06391ed69e80dd42168636d59907ce5
jeff-mccommas <jeffmccommas@gmail.com>
2018-03-10 17:29:56 -0500

    added toggle menu

--- server/server.js
+++ server/server.js
@@ -7,9 +7,17 @@ const cookieParser = require('cookie-parser');
 const moment = require('moment');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+const passport = require('passport');
+const flash = require('connect-flash');
+const _ = require('underscore');
 const app = express();
 const config = require('./config/config').get(process.env.NODE_ENV);
 ////######### HBS SETUP ############/////
+
+
+
+
+
 app.engine('hbs',hbs({
     extname:'hbs',
     defaultLayout: 'main',
@@ -23,7 +31,6 @@ mongoose.connect(config.DATABASE);
 
 
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
 app.use(session({
    secret:'bluebird',
@@ -34,8 +41,8 @@ app.use(session({
    })
 }))
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', function (req, res) {
+    res.render('home');
     
 });
 
